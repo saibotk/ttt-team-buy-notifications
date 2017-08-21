10766035900761b4a35f539f445fde7d0ece50d1
saibotk <tkindanight@gmail.com>
2017-08-22 01:53:39 +0200

    Initial Code Commit

---
+++ lua/autorun/client/cl_buy_notif_autorun.lua
@@ -0,0 +1,92 @@
+-- TTT TEAM EQUIPMENT BUY NOTIFICATION
+-- MADE BY saibotk (tkindanight)
+
+-- Create equip table for chache
+local tbl = nil
+
+-- Receive Callback
+net.Receive( "ItemBought", function()
+  local SafeTranslate = LANG.TryTranslation
+
+  -- Read sent information
+  local ply = net.ReadEntity()
+  local equipment = net.ReadString()
+  local is_item = net.ReadBool()
+
+-- Copy equipment table
+  if tbl == nil then
+    tbl = table.Copy( EquipmentItems )
+    for k,v in pairs( TTTRoles ) do
+      if v.ShopFallBack then -- Compat for TTT Totem by GamefreakDE
+        tbl[v.ID] = table.Copy( EquipmentItems[v.ShopFallBack] )
+      end
+    end
+  end
+
+-- Set defaults
+  local itemName = "Undefined"
+  local itemMaterial = "entities/npc_kleiner.png"
+
+  if is_item then
+    for _, item in pairs( tbl[ply:GetRole()] ) do
+      if item.id == tonumber( equipment ) and item.name and item.material then
+        itemName = SafeTranslate( item.name )
+        itemMaterial = item.material
+        break
+      end
+    end
+  else
+    local item = weapons.GetStored( equipment )
+    itemName = item.PrintName
+    itemMaterial = item.Icon
+  end
+
+  -- Fallback to prevent errors
+  if not itemName then itemName = "undefined" end
+  if not itemMaterial then itemMaterial = "entities/npc_kleiner.png" end
+
+  -- Create notification GUI
+  local notif = vgui.Create( "DNotify" )
+  notif:SetPos( 15, 15 )
+  notif:SetSize( 300, 86 )
+  notif:SetLife( 4 )
+
+  -- Create background panel
+  local bg = vgui.Create( "DPanel", notif )
+  bg:Dock(FILL)
+  local bgColor = Color( 255, 0, 0 )
+  if ply.GetRoleTable then
+    bgColor = ply:GetRoleTable().DefaultColor
+  else
+    bgColor = GetRoleTableByID(ply:GetRole()).DefaultColor
+  end
+  bgColor.a = 240
+  bg:SetBackgroundColor( bgColor )
+
+  -- Add icon GUI element
+  local img = vgui.Create( "DImage", bg )
+  img:SetPos( 11, 11 )
+  img:SetSize( 64, 64 )
+  img:SetImage( itemMaterial )
+
+  -- Add name label
+  local lblPlayerNick = vgui.Create( "DLabel", bg )
+  lblPlayerNick:SetPos( 86, 11 )
+  lblPlayerNick:SetSize( 203, 32 )
+  lblPlayerNick:SetText( ply:GetName() )
+  lblPlayerNick:SetTextColor( Color( 255, 250, 250 ) )
+  lblPlayerNick:SetFont( "Trebuchet24" )
+  lblPlayerNick:SetWrap( false )
+
+  -- Add item name label
+  local lblItemName = vgui.Create( "DLabel", bg )
+  lblItemName:SetPos( 86, 43 )
+  lblItemName:SetSize( 203, 32 )
+  lblItemName:SetText( itemName )
+  lblItemName:SetTextColor( Color( 255, 250, 250 ) )
+  lblItemName:SetFont( "HudHintTextLarge" )
+  lblItemName:SetWrap( false )
+
+  -- Add all to notification
+  notif:AddItem( bg )
+end )
---
+++ lua/autorun/server/buy_notif_server.lua
@@ -0,0 +1,25 @@
+-- TTT TEAM EQUIPMENT BUY NOTIFICATION
+-- MADE BY saibotk (tkindanight)
+local buyNotificationEnabled = CreateConVar("ttt_buy_notification","1", {FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Should TTT Buy Notifications be active?"):GetBool()
+util.AddNetworkString("ItemBought")
+
+local function SendBoughtNotif(ply_o, equipment, is_item)
+  if not buyNotificationEnabled then return end
+  -- Get player role and find other teammembers
+  local teammembers = {}
+  for _, ply in pairs( player.GetAll() ) do
+    -- Added Compat for TTT Totem by GamefreakDE
+     if IsValid( ply ) and ply:IsActive() and ( ( ply.GetTeam and ply:GetTeam() == ply_o:GetTeam() ) or ( ply:GetRole() == ply_o:GetRole() ) ) and ply != ply_o then
+        table.insert(teammembers, ply)
+     end
+  end
+
+  -- Send net message to teammembers to display the information
+  net.Start("ItemBought")
+  net.WriteEntity(ply_o)
+  net.WriteString(equipment)
+  net.WriteBool(is_item)
+  net.Send(teammembers)
+
+end
+hook.Add("TTTOrderedEquipment", "SendBoughtNotification", SendBoughtNotif)
