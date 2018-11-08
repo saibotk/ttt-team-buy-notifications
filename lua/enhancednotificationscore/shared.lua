6a5f46c4e7a050b140a822169c026db48a374f6b
Alf21 <4lf-mueller@gmx.de>
2018-11-08 19:41:07 +0100

    Update 1.3.1

--- lua/enhancednotificationscore/shared.lua
+++ lua/enhancednotificationscore/shared.lua
@@ -15,46 +15,29 @@
 --    You should have received a copy of the GNU General Public License
 --    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -------------------------------------------------------------------------------
-
 if SERVER then
 	AddCSLuaFile()
-	-- Create notification framework serverside
 	AddCSLuaFile("cl_init.lua")
 
-	if not ENHANCED_NOTIFICATIONS then
-		ENHANCED_NOTIFICATIONS = include("enhancednotificationscore/init.lua")
+	resource.AddFile("materials/vgui/ttt/tbn_ic_default")
+end
 
-		print("Loaded ENHANCED NOTIFICATIONS FRAMEWORK v" .. ENHANCED_NOTIFICATIONS:GetVersion())
+do
+	local initpath = SERVER and "enhancednotificationscore/init.lua" or "enhancednotificationscore/cl_init.lua"
+
+	if not ENHANCED_NOTIFICATIONS then
+		ENHANCED_NOTIFICATIONS = include(initpath)
 	else
-		local fwtoload = include("enhancednotificationscore/init.lua")
+		local fwtoload = include(initpath)
 		local fwtlver = fwtoload:GetVersion()
 		local fwalver = ENHANCED_NOTIFICATIONS:GetVersion()
 		local intfwvtl = tonumber(string.sub(fwtlver, 1, string.find(fwtlver, "%.", 1)) .. string.gsub(string.sub(fwtlver, string.find(fwtlver, "%.", 1) + 1), "%.", ""))
 		local intfwval = tonumber(string.sub(fwalver, 1, string.find(fwalver, "%.", 1)) .. string.gsub(string.sub(fwalver, string.find(fwalver, "%.", 1) + 1), "%.", ""))
 
 		if intfwval and intfwtl and intfwval < intfwvtl then
-			ENHANCED_NOTIFICATIONS = include("enhancednotificationscore/init.lua")
-
-			print("Loaded ENHANCED NOTIFICATIONS FRAMEWORK v" .. ENHANCED_NOTIFICATIONS:GetVersion())
+			ENHANCED_NOTIFICATIONS = include(initpath)
 		end
 	end
-else
-	-- Create notification framework clientside
-	if not ENHANCED_NOTIFICATIONS then
-		ENHANCED_NOTIFICATIONS = include("enhancednotificationscore/cl_init.lua")
 
-		print("Loaded ENHANCED NOTIFICATIONS FRAMEWORK v" .. ENHANCED_NOTIFICATIONS:GetVersion())
-	else
-		local fwtoload = include("enhancednotificationscore/cl_init.lua")
-		local fwtlver = fwtoload:GetVersion()
-		local fwalver = ENHANCED_NOTIFICATIONS:GetVersion()
-		local intfwvtl = tonumber(string.sub(fwtlver, 1, string.find(fwtlver, "%.", 1)) .. string.gsub(string.sub(fwtlver, string.find(fwtlver, "%.", 1) + 1), "%.", ""))
-		local intfwval = tonumber(string.sub(fwalver, 1, string.find(fwalver, "%.", 1)) .. string.gsub(string.sub(fwalver, string.find(fwalver, "%.", 1) + 1), "%.", ""))
-
-		if intfwval and intfwtl and intfwval < intfwvtl then
-			ENHANCED_NOTIFICATIONS = include("enhancednotificationscore/cl_init.lua")
-
-			print("Loaded ENHANCED NOTIFICATIONS FRAMEWORK v" .. ENHANCED_NOTIFICATIONS:GetVersion())
-		end
-	end
+	print("Loaded ENHANCED NOTIFICATIONS FRAMEWORK v" .. ENHANCED_NOTIFICATIONS:GetVersion())
 end
